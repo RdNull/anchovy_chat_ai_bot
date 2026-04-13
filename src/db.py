99c5e69b15c07c9001a6a3b867d91218a707674f
anton.luboshnikov <anton.luboshnikov@lionsoul.com>
2026-04-13 22:51:06 +0500

    feat: add `embedding_tasks` collection and related env setting for managing embedding task data

--- src/db.py
+++ src/db.py
@@ -9,6 +9,7 @@ __all__ = (
     'memory',
     'chats',
     'media_descriptions',
+    'embedding_tasks',
 )
 
 db_client = AsyncMongoClient(settings.DATABASE_URL)
@@ -18,3 +19,4 @@ recaps: AsyncCollection = db_client.data.recaps
 memory: AsyncCollection = db_client.data.memory
 chats: AsyncCollection = db_client.data.chats
 media_descriptions: AsyncCollection = db_client.data.media_descriptions
+embedding_tasks: AsyncCollection = db_client.data.embedding_tasks
