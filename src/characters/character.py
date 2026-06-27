09acde7e15642b2ee71cf0bc364f78e543d4095e
anton.luboshnikov <anton.luboshnikov@lionsoul.com>
2026-06-27 23:20:39 +0500

    chore: update character setup to use v7 and streamline LLM version selection

--- src/characters/character.py
+++ src/characters/character.py
@@ -52,7 +52,7 @@ class Character:
     def system_message(self):
         setup_prompt = prompt_manager.get_prompt(
             'character_setup',
-            version='v6',
+            version='v7',
             character_description=self.style_prompt,
             memory=self.memory.content.prompt_format() if self.memory else None,
             related_messages=self.related_messages or None,
@@ -70,7 +70,7 @@ class Character:
         if self.rate_limiter.is_exceeded(chat_id):
             return None
 
-        llm = self._get_llm(versions=('v7-cheap', 'v6'))
+        llm = self._get_llm(versions=('v7-cheap',))
         messages = [
             self.system_message,
             *_format_previous_messages(last_messages),
